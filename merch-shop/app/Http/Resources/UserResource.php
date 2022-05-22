843b4b7164a6376e8bc201d272f07638b5156906
SashaVasal <34645577+SashaVasal@users.noreply.github.com>
2022-05-22 18:43:58 +1000

    Lesson 5 (#16)
    * Lesson 4: Add appeal form (web & api)
    
    (cherry picked from commit 907691c50cee41832ffd4d8fd7914cc9f59a7c4d)
    
    * fix controller
    
    * fix controller
    
    * lesson 5

---
+++ merch-shop/app/Http/Resources/UserResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+/**
+ * @mixin User
+ */
+class UserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  Request  $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        return [
+            'name' => $this->name,
+            'email' => $this->email
+        ];
+    }
+}
